0bc4ddd2bfe78b9628c1f33aea4083d498a2a2d6
dnaumov <dnaumov@acumatica.com>
2021-02-09 08:55:11 -0500

    mprove form layout

--- DebuggerDisplayForm/DisplayForm.Designer.cs
+++ DebuggerDisplayForm/DisplayForm.Designer.cs
@@ -45,9 +45,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.pictureBox1.Location = new System.Drawing.Point(12, 49);
+			this.pictureBox1.Location = new System.Drawing.Point(9, 40);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(683, 429);
+			this.pictureBox1.Size = new System.Drawing.Size(681, 399);
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -57,43 +58,48 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Location = new System.Drawing.Point(9, 7);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(60, 16);
+			this.label1.Size = new System.Drawing.Size(48, 13);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Element:";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 30);
+			this.label2.Location = new System.Drawing.Point(9, 24);
+			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(46, 16);
+			this.label2.Size = new System.Drawing.Size(37, 13);
 			this.label2.TabIndex = 2;
 			this.label2.Text = "Value:";
 			// 
 			// ValueLabel
 			// 
 			this.ValueLabel.AutoSize = true;
-			this.ValueLabel.Location = new System.Drawing.Point(78, 30);
+			this.ValueLabel.Location = new System.Drawing.Point(58, 24);
+			this.ValueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.ValueLabel.Name = "ValueLabel";
 			this.ValueLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
-			this.ValueLabel.Size = new System.Drawing.Size(0, 16);
+			this.ValueLabel.Size = new System.Drawing.Size(0, 13);
 			this.ValueLabel.TabIndex = 3;
 			// 
 			// AddressLabel
 			// 
 			this.AddressLabel.AutoSize = true;
-			this.AddressLabel.Location = new System.Drawing.Point(78, 9);
+			this.AddressLabel.Location = new System.Drawing.Point(58, 7);
+			this.AddressLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.AddressLabel.Name = "AddressLabel";
-			this.AddressLabel.Size = new System.Drawing.Size(0, 16);
+			this.AddressLabel.Size = new System.Drawing.Size(0, 13);
 			this.AddressLabel.TabIndex = 4;
 			// 
 			// numericUpDown1
 			// 
-			this.numericUpDown1.Location = new System.Drawing.Point(405, 13);
+			this.numericUpDown1.Location = new System.Drawing.Point(304, 11);
+			this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.numericUpDown1.Name = "numericUpDown1";
-			this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown1.Size = new System.Drawing.Size(90, 20);
 			this.numericUpDown1.TabIndex = 5;
 			this.numericUpDown1.Value = new decimal(new int[] {
             5,
@@ -105,17 +111,18 @@
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(339, 15);
+			this.label3.Location = new System.Drawing.Point(254, 12);
+			this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(46, 16);
+			this.label3.Size = new System.Drawing.Size(37, 13);
 			this.label3.TabIndex = 6;
 			this.label3.Text = "Scale:";
 			// 
 			// DisplayForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(707, 490);
+			this.ClientSize = new System.Drawing.Size(698, 448);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.numericUpDown1);
 			this.Controls.Add(this.AddressLabel);
@@ -123,6 +130,7 @@
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Name = "DisplayForm";
 			this.Text = "DisplayForm";
 			this.Paint += new System.Windows.Forms.PaintEventHandler(this.DisplayForm_Paint);
